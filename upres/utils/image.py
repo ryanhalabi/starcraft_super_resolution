7cba7b9c232cfc17f6b9629900dd777a275ddbd1
Ryan Halabi <ryanhalabi@gmail.com>
2020-03-11 22:29:44 -0600

    ws fix

--- upres/utils/image.py
+++ upres/utils/image.py
@@ -59,7 +59,7 @@ def download_images(urls):
     for url in urls:
         # unit = re.search(r"[/\d]([A-Za-z_]*).png", url).group(1)
         unit = re.search(r"[/\d]([\w]*).jpg", url).group(1)
-       file_name = str(env.units / f"{unit}.png")
+        file_name = str(env.units / f"{unit}.png")
         with open(file_name, "wb+") as f:
             f.write(requests.get(url).content)
 
